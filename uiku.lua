d35ad41830e4cd9e43f893244a8818fe7fe433f9
radityans <radityanugrohosaputro@gmail.com>
2025-09-20 02:12:23 +0700

    drag

--- uiku.lua
+++ uiku.lua
@@ -57,14 +57,13 @@ function Framework:CreateWindow(Settings)
     local function adjustSize()
         local viewport = workspace.CurrentCamera.ViewportSize
         if viewport.X < 800 then
-            -- 📱 Mobile (portrait)
-            Main.Size = UDim2.new(0.9, 0, 0.6, 0) -- lebar 90%, tinggi 60%
+            -- 📱 Mobile (portrait): ambil hampir full layar
+            Main.Size = UDim2.new(0.9, 0, 0.7, 0)
         else
-            -- 💻 PC
-            Main.Size = UDim2.new(0.6, 0, 0.7, 0) -- lebar 60%, tinggi 70%
+            -- 💻 PC: ukuran lebih elegan (ga full)
+            Main.Size = UDim2.new(0.5, 0, 0.5, 0)
         end
     end
-
 
     adjustSize()
     workspace.CurrentCamera:GetPropertyChangedSignal("ViewportSize"):Connect(adjustSize)
