ee95c8ca1c74ad01055d7ae8cd02899b0c966df6
radityans <radityanugrohosaputro@gmail.com>
2025-09-20 01:59:46 +0700

    drag

--- uiku.lua
+++ uiku.lua
@@ -231,7 +231,7 @@ function Framework:CreateWindow(Settings)
     -- Garis pemisah antara tab dan konten
     local Separator = Instance.new("Frame")
     Separator.Size = UDim2.new(1, -20, 0, 1) -- lebar penuh, tinggi 1px
-    Separator.Position = UDim2.new(0, 10, 0, 80) -- pas di bawah TabButtons
+    Separator.Position = UDim2.new(0, 10, 0, TabButtons.Position.Y.Offset + TabButtons.Size.Y.Offset + 10)
     Separator.BackgroundColor3 = Color3.fromRGB(120, 120, 120) -- silver gelap
     Separator.BorderSizePixel = 0
     Separator.Parent = Main
